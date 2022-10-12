04ffe977eb08ce68c828f2478d85231c2a38a85d
Zhelini Sivanesan <zhelini.sivanesan@satellitevu.com>
2022-10-12 16:41:13 +0100

    add interface to orders api

---
+++ satellitevu/apis/orders.py
@@ -0,0 +1,15 @@
+from typing import List, Union
+
+from .base import AbstractApi
+
+
+class OrdersV1(AbstractApi):
+    api_path = "orders/v1"
+
+    def submit(self, item_ids: Union[List[str], str]):
+        url = self._url("/")
+
+        if isinstance(item_ids, str):
+            item_ids = [item_ids]
+
+        return self.client.post(url=url, json={"item_id": item_ids})
