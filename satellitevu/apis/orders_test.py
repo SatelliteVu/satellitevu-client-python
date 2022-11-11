8ee4cd960dc73c5331931245e7b20f0709221210
Zhelini Sivanesan <zhelini.sivanesan@satellitevu.com>
2022-11-11 12:05:01 +0000

    add tests into orders api for 401/403 error handling

--- satellitevu/apis/orders_test.py
+++ satellitevu/apis/orders_test.py
@@ -4,11 +4,13 @@ from json import dumps
 from unittest.mock import patch
 from urllib.parse import urljoin, urlparse
 
+import pytest
 from mocket import Mocket, mocketize
 from mocket.mockhttp import Entry
 from pytest import mark
 
 from satellitevu.apis.orders import bytes_to_file
+from satellitevu.auth.exc import Api401Error, Api403Error
 
 
 @mocketize(strict_mode=True)
@@ -68,6 +70,43 @@ def test_submit_multiple_items(client, oauth_token_entry, item_ids):
 
 
 @mocketize(strict_mode=True)
+@mark.parametrize(
+    "item_ids, status, exception",
+    (
+        ("20221005T214049000_basic_0_TABI", 401, Api401Error),
+        (
+            ["20221005T214049000_basic_0_TABI", "20220923T222227000_basic_0_TABI"],
+            403,
+            Api403Error,
+        ),
+    ),
+)
+def test_cannot_submit_unauthorized_order(
+    client, oauth_token_entry, item_ids, status, exception
+):
+    if isinstance(item_ids, str):
+        item_ids = [item_ids]
+    payload = dumps({"item_id": item_ids})
+
+    Entry.single_register(
+        "POST", client._gateway_url + "orders/v1/", body=payload, status=status
+    )
+
+    with pytest.raises(exception):
+        client.orders_v1.submit(item_ids)
+
+    requests = Mocket.request_list()
+    assert len(requests) == 2
+
+    api_request = requests[-1]
+    assert api_request.headers["Host"] == urlparse(client._gateway_url).hostname
+    assert api_request.path == "/orders/v1/"
+    assert api_request.headers["Content-Type"] == "application/json"
+    assert api_request.headers["Authorization"] == "Bearer mock-token"
+    assert api_request.body == payload
+
+
+@mocketize(strict_mode=True)
 def test_item_download_url(client, oauth_token_entry, redirect_response):
     order_id = "uuid"
     item_id = "image"
@@ -89,6 +128,33 @@ def test_item_download_url(client, oauth_token_entry, redirect_response):
 
     assert isinstance(response, dict)
     assert response["url"] == "https://image.test"
+
+
+@mocketize(strict_mode=True)
+@mark.parametrize("status, exception", ((401, Api401Error), (403, Api403Error)))
+def test_no_access_to_download_if_unauthorized(
+    client, oauth_token_entry, status, exception, redirect_response
+):
+    order_id = "uuid"
+    item_id = "image"
+
+    Entry.single_register(
+        "GET",
+        client._gateway_url + f"orders/v1/{order_id}/{item_id}/download?redirect=False",
+        body=dumps(redirect_response),
+        status=status,
+    )
+
+    with pytest.raises(exception):
+        client.orders_v1.item_download_url(order_id, item_id)
+
+    requests = Mocket.request_list()
+
+    assert len(requests) == 2
+    api_request = requests[-1]
+    assert api_request.headers["Host"] == urlparse(client._gateway_url).hostname
+    assert api_request.path == "/orders/v1/uuid/image/download?redirect=False"
+    assert api_request.headers["Authorization"] == "Bearer mock-token"
 
 
 @mocketize(strict_mode=True)
@@ -147,6 +213,32 @@ def test_get_order_details(client, oauth_token_entry, order_details_response):
 
 
 @mocketize(strict_mode=True)
+@mark.parametrize("status, exception", ((401, Api401Error), (403, Api403Error)))
+def test_cannot_get_order_details_if_unauthorized(
+    client, oauth_token_entry, status, exception, order_details_response
+):
+    fake_uuid = "528b0f77-5df1-4ed7-9224-502817170613"
+
+    Entry.single_register(
+        "GET",
+        client._gateway_url + f"orders/v1/{fake_uuid}",
+        body=dumps(order_details_response),
+        status=status,
+    )
+
+    with pytest.raises(exception):
+        client.orders_v1.get_order_details(fake_uuid)
+
+    requests = Mocket.request_list()
+    assert len(requests) == 2
+
+    api_request = requests[-1]
+    assert api_request.headers["Host"] == urlparse(client._gateway_url).hostname
+    assert api_request.path == f"/orders/v1/{fake_uuid}"
+    assert api_request.headers["Authorization"] == "Bearer mock-token"
+
+
+@mocketize(strict_mode=True)
 def test_download_order(client, order_details_response, redirect_response):
     fake_uuid = "528b0f77-5df1-4ed7-9224-502817170613"
     download_dir = "downloads"
@@ -182,6 +274,40 @@ def test_download_order(client, order_details_response, redirect_response):
     assert response == mock_zip()
 
     Mocket.assert_fail_if_entries_not_served()
+
+
+@mocketize(strict_mode=True)
+@mark.parametrize("status, exception", ((401, Api401Error), (403, Api403Error)))
+def test_download_order_unauthorized(
+    client, order_details_response, redirect_response, status, exception
+):
+    fake_uuid = "528b0f77-5df1-4ed7-9224-502817170613"
+    download_dir = "downloads"
+
+    Entry.single_register(
+        "POST",
+        urljoin(client.auth.auth_url, "oauth/token"),
+        body=dumps({"access_token": None}),
+    )
+
+    Entry.single_register(
+        "GET",
+        client._gateway_url + f"orders/v1/{fake_uuid}",
+        body=dumps({**order_details_response, **{"access_token": None}}),
+        status=status,
+    )
+
+    with pytest.raises(exception):
+        client.orders_v1.download_order(fake_uuid, download_dir)
+
+    requests = Mocket.request_list()
+
+    assert len(requests) == 2
+
+    api_request = requests[1]
+    assert api_request.headers["Host"] == urlparse(client._gateway_url).hostname
+    assert api_request.path == f"/orders/v1/{fake_uuid}"
+    assert api_request.headers["Authorization"] == "Bearer None"
 
 
 def test_bytes_to_file():
