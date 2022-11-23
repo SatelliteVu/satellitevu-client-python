4533f23daf5a4a5c8d57a3f1d215ae74415950be
Christian Wygoda <christian.wygoda@satellitevu.com>
2022-11-23 10:18:52 +0100

    Make release optional

---
+++ .github/version.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from re import compile
+from subprocess import run
+from sys import stderr
+
+RULE = compile(r"^(?P<rule>(patch|minor|major))\/.*$")
+
+
+def get_bump_rule(branch: str):
+    match = RULE.match(branch)
+    if match:
+        return match.groupdict()["rule"]
+
+
+def bump_version(rule):
+    run(["poetry", "version", rule])
+    return run(["poetry", "version", "-s"])
+
+
+def main(branch):
+    rule = get_bump_rule(branch)
+    if rule:
+        new_version = bump_version(rule)
+        print(f"NEW_VERSION={new_version}")
+    else:
+        print("No bump rule detected, skipping", file=stderr)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("branch")
+
+    args = parser.parse_args()
+    main(args.branch)
