726ad54fc1950a7bff41cc279fac0789bea96c68
James Harrison <james.harrison@satellitevu.com>
2023-05-24 15:21:45 +0100

    Add some feasibility, order and price otm endpoints

--- satellitevu/apis/otm.py
+++ satellitevu/apis/otm.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 from uuid import UUID
 
 from .base import AbstractApi
@@ -49,4 +49,96 @@ class OtmV1(AbstractApi):
         return self.make_request(
             method="GET",
             url=self.url(f"/tasking/feasibilities/{str(id)}"),
+        )
+
+    def get_feasibility_response(self, *, id: Union[UUID, str]):
+        """
+        TODO: Docs
+        """
+        return self.make_request(
+            method="GET",
+            url=self.url(f"/tasking/feasibilities/{str(id)}/response"),
+        )
+
+    def list_feasibility_requests(
+        self, *, per_page: int = 25, page_token: Optional[str] = None
+    ):
+        """
+        TODO: Docs
+        """
+        query = f"per_page={per_page}"
+        if page_token:
+            query += f"&token={page_token}"
+
+        return self.make_request(
+            method="GET", url=self.url(f"/tasking/feasibilities/?{query}")
+        )
+
+    def post_order(
+        self,
+        *,
+        coordinates: Union[Tuple[float, float], Tuple[float, float, float]],
+        date_from: datetime,
+        date_to: datetime,
+    ):
+        """
+        TODO: Docs
+        """
+        url = self.url("/tasking/orders/")
+        payload = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": coordinates,
+            },
+            "properties": {
+                "datetime": f"{date_from.isoformat()}/{date_to.isoformat()}"
+            },
+        }
+
+        return self.make_request(
+            method="POST", url=url, json={k: v for k, v in payload.items() if v}
+        )
+
+    def get_order(self, *, id: Union[UUID, str]):
+        """
+        TODO: Docs
+        """
+        return self.make_request(
+            method="GET", url=self.url(f"/tasking/orders/{str(id)}")
+        )
+
+    def list_orders(self, *, per_page: int = 25, page_token: Optional[str] = None):
+        """
+        TODO: Docs
+        """
+        query = f"per_page={per_page}"
+        if page_token:
+            query += f"&token={page_token}"
+
+        return self.make_request(
+            method="GET", url=self.url(f"/tasking/orders/?{query}")
+        )
+
+    def get_price(
+        self,
+        *,
+        coordinates: Union[Tuple[float, float], Tuple[float, float, float]],
+        date_from: datetime,
+        date_to: datetime,
+    ):
+        url = self.url("/tasking/price/")
+        payload = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": coordinates,
+            },
+            "properties": {
+                "datetime": f"{date_from.isoformat()}/{date_to.isoformat()}"
+            },
+        }
+
+        return self.make_request(
+            method="POST", url=url, json={k: v for k, v in payload.items() if v}
         )
