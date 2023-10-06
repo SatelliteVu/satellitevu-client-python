e456069f38c75dbbc67dca6400aadba5751bba48
Zhelini Sivanesan <zhelini.sivanesan@satellitevu.com>
2023-10-06 12:58:28 +0100

    pass in auth object to instantiate contracts api

--- satellitevu/apis/contracts.py
+++ satellitevu/apis/contracts.py
@@ -1,22 +1,45 @@
+from uuid import UUID
+from typing import Union
+
+from satellitevu.http import AbstractClient
+from satellitevu.auth import Auth
 from .base import AbstractApi
 
 
 class ContractsV1(AbstractApi):
     """
-    Interface to accessing the contracts and products which a customer has access to.
+    Interface for information about the contracts and associated products a
+    customer has access to.
     """
 
     api_path = "policy/v1"
+
+    def __init__(self, client: AbstractClient, base_url: str, auth: Auth):
+        super().__init__(client, base_url)
+        self._auth = auth
 
     def get_contracts(self):
         url = self.url("/contracts")
         response = self.make_request(
             method="POST",
             url=url,
-            json={"token": self.client.h},
+            json={"token": self._auth.token()},
         )
 
         if response.status != 200:
             raise Exception(f"Error - {response.status} : {response.text}")
 
-        return response.json()
+        return response.json()["result"]
+
+    def get_contract_pricebook(self, contract_id: Union[UUID, str]):
+        url = self.url("/policy/query/products")
+        response = self.make_request(
+            method="POST",
+            url=url,
+            json={"token": self._auth.token(), "contract_id": str(contract_id)},
+        )
+
+        if response.status != 200:
+            raise Exception(f"Error - {response.status} : {response.text}")
+
+        return response.json()["result"]
