8c0448df829ae8956a5045d9850494c7f812ccd6
Zhelini Sivanesan <zhelini.sivanesan@satellitevu.com>
2023-11-14 17:54:15 +0000

    update otm example

--- examples/otm.py
+++ examples/otm.py
@@ -23,10 +23,11 @@ def _setup_client():
     return Client(client_id=client_id, client_secret=client_secret)
 
 
-def feasibility():
+def feasibility(contract_id):
     client = _setup_client()
     now = datetime.utcnow()
-    response = client.future.otm.post_feasibility(
+    response = client.otm_v2.post_feasibility(
+        contract_id=contract_id,
         coordinates=[12, 52],
         date_from=now,
         date_to=now + timedelta(days=5),
@@ -42,7 +43,7 @@ def feasibility():
     while status == "pending":
         sleep(2)
         print("### Polling status…", end=" ")
-        response = client.future.otm.get_feasibility(id=fid)
+        response = client.otm_v2.get_feasibility(contract_id=contract_id, id=fid)
         data = response.json()
         status = data["properties"]["status"]
         print(f"{status} ###")
@@ -53,6 +54,7 @@ def feasibility():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("contract_id", help="A Contract ID must be provided.")
     parser.add_argument(
         "--verbose", help="increase output verbosity", action="store_true"
     )
@@ -60,4 +62,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     setup_logging(args.verbose)
 
-    feasibility()
+    feasibility(args.contract_id)
