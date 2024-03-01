ecee56107fa969ef8c383262b525549a848c365a
Zhelini Sivanesan <zhelini.sivanesan@satellitevu.com>
2024-03-01 11:37:05 +0000

    add custom contracts exception

--- satellitevu/apis/exceptions.py
+++ satellitevu/apis/exceptions.py
@@ -1,3 +1,9 @@
+class ContractAccessError(Exception):
+    def __init__(self, status_code: int, detail: str) -> None:
+        self.message = f"Contracts Access Error - {status_code} : {detail}"
+        super().__init__(self.message)
+
+
 class OTMError(Exception):
     def __init__(self, status_code: int, detail: str) -> None:
         self.message = f"OTM API Error - {status_code} : {detail}"
