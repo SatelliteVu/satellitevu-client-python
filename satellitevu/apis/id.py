92afbe110cf1ab263e22e8d38f2efbfe3c1b959d
James Harrison <james.harrison@satellitevu.com>
2024-11-11 17:32:41 +0000

    feat(IdV2): support ID API webhook endpoints
    * POST create webhook
    * GET webhook
    * GET list webhooks
    * PATCH edit webhook
    * DELETE webhook
    * POST rotate webhook signing key
    * POST test webhook
    * GET webhook events

--- satellitevu/apis/id.py
+++ satellitevu/apis/id.py
@@ -1,4 +1,4 @@
-from typing import Any, Union, Dict
+from typing import Any, Union, Dict, List, Optional
 from uuid import UUID
 
 from satellitevu.apis.base import AbstractApi
@@ -56,4 +56,199 @@ class IdV2(AbstractApi):
         response = self.make_request(method="GET", url=url)
         if response.status != 200:
             raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def create_webhook(
+        self,
+        name: str,
+        url: str,
+        event_types: List[str],
+        **kwargs,
+    ):
+        """
+        Create a webhook.
+
+        Args:
+            name: The name of the webhook.
+
+            url: The URL where you want to receive requests for events you are subscribed to.
+            Must be HTTPS.
+
+            event_types: A list of events to subscribe to.
+
+        Kwargs:
+            Allows sending additional parameters that are supported by the API but not
+            added to this SDK yet.
+
+        Returns:
+            A dictionary containing properties of the created webhook.
+        """
+        payload = {
+            "event_types": event_types,
+            "name": name,
+            "url": url,
+            **kwargs,
+        }
+
+        response = self.make_request(
+            method="POST",
+            url=self.url("webhooks/"),
+            json=payload,
+        )
+
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def get_webhook(self, webhook_id: Union[UUID, str]):
+        """
+        Get information about an existing webhook.
+
+        Args:
+            webhook_id: The webhook ID.
+
+        Returns:
+            A dictionary containing properties of the webhook.
+        """
+        response = self.make_request(
+            method="GET",
+            url=self.url(f"webhooks/{str(webhook_id)}/"),
+        )
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def list_webhooks(
+        self,
+        per_page: int = 25,
+        page_token: Optional[str] = None,
+    ):
+        """
+        List all webhooks.
+
+        Args:
+            per_page: Number of results (defaults to 25) to be returned per page.
+
+            page_token: Optional string key used to return specific page of results.
+            Defaults to None -> assumes page 0.
+
+        Returns:
+            A list of dictionaries containing properties of each webhook.
+        """
+
+        url = self.url(f"webhooks/?per_page={per_page}")
+        if page_token:
+            url += f"&token={page_token}"
+
+        response = self.make_request(method="GET", url=url)
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def edit_webhook(
+        self,
+        webhook_id: Union[UUID, str],
+        active: Optional[bool] = None,
+        event_types: Optional[List[str]] = None,
+        name: Optional[str] = None,
+        **kwargs,
+    ):
+        """
+        Edit a webhook.
+
+        Args:
+            webhook_id: The webhook ID.
+
+            active: Whether the webhook should be active or not.
+
+            event_types: A list of events to subscribe to.
+
+            name: The name of the webhook.
+
+        Kwargs:
+            Allows sending additional parameters that are supported by the API but not
+            added to this SDK yet.
+
+        Returns:
+            A dictionary containing properties of the webhook.
+        """
+        payload = {**kwargs}
+
+        if active:
+            payload["active"] = active
+        if event_types:
+            payload["event_types"] = event_types
+        if name:
+            payload["name"] = name
+
+        response = self.make_request(
+            method="PATCH",
+            url=self.url(f"webhooks/{webhook_id}/"),
+            json=payload,
+        )
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def delete_webhook(self, webhook_id: Union[UUID, str]):
+        """
+        Delete a webhook.
+
+        Args:
+            webhook_id: The webhook ID.
+        """
+
+        response = self.make_request(
+            method="DELETE",
+            url=self.url(
+                f"webhooks/{webhook_id}/",
+            ),
+        )
+        if response.status != 204:
+            raise IDAPIError(response.status, response.text)
+
+    def rotate_webhook_signing_key(self, webhook_id: Union[UUID, str]):
+        """
+        Rotate the signing key for a webhook.
+
+        Args:
+            webhook_id: The webhook ID.
+
+        Returns:
+            A dictionary containing properties of the webhook,
+            including the new signing key.
+        """
+        response = self.make_request(
+            method="POST", url=self.url(f"webhooks/{webhook_id}/rotate/")
+        )
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def test_webhook(self, webhook_id: Union[UUID, str]):
+        """
+        Test a webhook.
+
+        Args:
+            webhook_id: The webhook ID.
+
+        Returns:
+            A dictionary containing properties of the webhook
+            and details about the result of the test.
+        """
+        response = self.make_request(
+            method="POST", url=self.url(f"/webhooks/{webhook_id}/test/")
+        )
+        if response.status != 200:
+            raise IDAPIError(response.status, response.text)
+        return response.json()
+
+    def get_webhook_events(self):
+        """
+        Retrieves all webhook event types.
+
+        Returns:
+            A list of all webhook event types.
+        """
+        response = self.make_request(method="GET", url=self.url("webhooks/events/"))
         return response.json()
